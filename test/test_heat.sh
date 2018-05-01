c3fe588ec75fc9c0c3bf098e64c050532efa0e90
gvdoord <g.vandenOord@gmail.com>
2018-05-01 16:17:53 +0200

    Fixed test script

--- test/test_heat.sh
+++ test/test_heat.sh
@@ -2,5 +2,6 @@
 
 echo "Please note for this test to run you need to install bmi with the bmi_heat example"
 trap "kill 0" SIGINT
-python run_server.py --name heat.BmiHeat &
-bmi-tester bmi_grpc_client.BmiClient
+run-bmi-server --name heat.BmiHeat &
+sleep 1 
+bmi-tester grpc4bmi.bmi_grpc_client.BmiClient
