961200feb268ca1bab9d11decb1ff4f790cf4240
gvdoord <g.vandenOord@gmail.com>
2018-05-08 17:59:58 +0200

    Added mount volumes.

--- grpc4bmi/bmi_client_docker.py
+++ grpc4bmi/bmi_client_docker.py
@@ -5,17 +5,18 @@ from grpc4bmi.bmi_grpc_client import BmiClient
 
 class BmiClientDocker(BmiClient):
 
-    def __init__(self, image, module=None):
+    def __init__(self, image, host=None, input_dir=None, output_dir=None):
         client = docker.from_env()
         port = BmiClient.get_unique_port()
-        # internal_port = BmiClient.start_port
-        # args = ["run-bmi-server", "--port", str(internal_port)]
-        # if module is not None:
-        #     args.extend(["--name", module])
-        # ports = {'/'.join([str(internal_port), "tcp"]): port}
-
-        self.container = client.containers.run(image, ports={"50051/tcp": port}, detach=True)
-        super(BmiClientDocker, self).__init__(BmiClient.create_grpc_channel(port=port))
+        volumes = {}
+        if input_dir is not None:
+            volumes[input_dir] = {"bind": "/data/input", "mode": "ro"}
+        if output_dir is not None:
+            volumes[output_dir] = {"bind": "/data/output", "mode": "rw"}
+        self.container = client.containers.run(image, ports={"50051/tcp": port},
+                                               volumes=volumes,
+                                               detach=True)
+        super(BmiClientDocker, self).__init__(BmiClient.create_grpc_channel(port=port, host=host))
 
     def __del__(self):
         self.container.stop()
