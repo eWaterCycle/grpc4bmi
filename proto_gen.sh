543c54f6fc9a3b6fb56ab55f4d8c51e117a8ccfd
gvdoord <g.vandenOord@gmail.com>
2018-08-20 18:17:27 +0200

    Fixed proto_gen paths

--- proto_gen.sh
+++ proto_gen.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-protoc -I ./proto --grpc_out=./cpp/grpc4bmi --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ./proto/bmi.proto
-protoc -I ./proto --cpp_out=./cpp/grpc4bmi ./proto/bmi.proto
-python -m grpc_tools.protoc -I./proto --python_out=./grpc4bmi --grpc_python_out=./python/grpc4bmi ./proto/bmi.proto
+protoc -I ./proto --grpc_out=./cpp --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ./proto/bmi.proto
+protoc -I ./proto --cpp_out=./cpp ./proto/bmi.proto
+python -m grpc_tools.protoc -I./proto --python_out=./python/grpc4bmi --grpc_python_out=./python/grpc4bmi ./proto/bmi.proto
