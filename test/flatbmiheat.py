59539401d657c04ded6ec6795f6888a7b83d0004
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2019-07-15 10:29:24 +0200

    Test against legacy bmi heat model

--- test/flatbmiheat.py
+++ test/flatbmiheat.py
@@ -3,7 +3,7 @@ from heat import BmiHeat
 
 
 class FlatBmiHeat(BmiHeat):
-    """The BmiHeat returns multi dimensional arrays, but
+    """The BmiHeat returns multi dimensional arrays, but grpc interface uses flat arrays
 
     """
     def get_value_at_indices(self, var_name, dest, indices):
@@ -37,3 +37,22 @@ class FlatBmiHeat(BmiHeat):
         val = super(FlatBmiHeat, self).get_grid_origin(grid_id)
         numpy.copyto(src=val, dst=dest)
         return dest
+
+
+class FlatLegacyBmiHeat(BmiHeat):
+    """BMI implementing interface defined in basic-modeling-interface 0.2
+
+    https://pypi.org/project/basic-modeling-interface/0.2/
+
+    """
+    def set_value(self, var_name, src):
+        shape = super(FlatLegacyBmiHeat, self).get_grid_shape(super(FlatLegacyBmiHeat, self).get_var_grid(var_name))
+        reshaped = numpy.reshape(src, shape)
+        super(FlatLegacyBmiHeat, self).set_value(var_name, reshaped)
+
+    def get_value(self, var_name):
+        val = super(FlatLegacyBmiHeat, self).get_value(var_name)
+        return val.flatten()
+
+    def get_value_at_indices(self, var_name, indices):
+        return super(FlatLegacyBmiHeat, self).get_value_at_indices(var_name, indices).flatten()
