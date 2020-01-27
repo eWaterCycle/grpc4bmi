1ddd713f10ab7a99fbce94a4f6257e7c3440426d
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2020-01-27 14:11:32 +0100

    Add logs to DeadDockerContainerException message + raise NotADirectoryError when input dir does not exist
    Refs #50

--- test/test_docker.py
+++ test/test_docker.py
@@ -42,6 +42,12 @@ class TestBmiClientDocker:
         with pytest.raises(NotImplementedError):
             walrus_model.get_value_ref('Q')
 
+    def test_inputdir_absent(self, tmp_path):
+        dirthatdoesnotexist = 'dirthatdoesnotexist'
+        input_dir = tmp_path / dirthatdoesnotexist
+        with pytest.raises(NotADirectoryError, match=dirthatdoesnotexist):
+            BmiClientDocker(image=walrus_docker_image, image_port=55555, input_dir=str(input_dir))
+
     def test_container_start_failure(self, exit_container):
         expected = r"Failed to start Docker container with image"
         with pytest.raises(DeadDockerContainerException, match=expected) as excinfo:
