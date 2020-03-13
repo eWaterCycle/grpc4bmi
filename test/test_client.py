3c7c078d1cead49379ccde5ce46102033ac97a25
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-03-13 10:24:23 +0100

    Added tests for create_grpc_channel
    Refs #63

--- test/test_client.py
+++ test/test_client.py
@@ -260,3 +260,21 @@ def test_get_grid_points():
     assert numpy.array_equal(client.get_grid_x(grid_id), local_x)
     assert numpy.array_equal(client.get_grid_y(grid_id), local_y)
     assert numpy.array_equal(client.get_grid_z(grid_id), local_z)
+
+
+class TestCreateGrpcChannel:
+    def test_defaults(self):
+        with BmiClient.create_grpc_channel() as channel:
+            target = channel._channel.target()
+            assert target == b'localhost:50051'
+
+    def test_custom(self):
+        with BmiClient.create_grpc_channel(51234, 'somehost') as channel:
+            target = channel._channel.target()
+            assert target == b'somehost:51234'
+
+    def test_same_port_twice(self):
+        port = 51235
+        with BmiClient.create_grpc_channel(port) as channel1, BmiClient.create_grpc_channel(port) as channel2:
+            assert channel1._channel.target() == b'localhost:51235'
+            assert channel2._channel.target() == b'localhost:51235'
