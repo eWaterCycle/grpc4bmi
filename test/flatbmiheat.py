9445f28b3121ce7fce8fca5d4b2186dd0bc88f0b
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2021-01-26 17:06:29 +0100

    Implement legacy methods of heat model

--- test/flatbmiheat.py
+++ test/flatbmiheat.py
@@ -1,43 +1,18 @@
 import numpy
 from heat import BmiHeat
 
+from grpc4bmi.reserve import reserve_values, reserve_values_at_indices, reserve_grid_shape, reserve_grid_padding
+
 
 class FlatBmiHeat(BmiHeat):
     """The BmiHeat returns multi dimensional arrays, but grpc interface uses flat arrays
 
     """
-    def get_value_at_indices(self, var_name, dest, indices):
-        val = super(FlatBmiHeat, self).get_value_at_indices(var_name, dest, indices).flatten()
-        numpy.copyto(src=val, dst=dest)
-        return dest
-
     def set_value(self, var_name, src):
         grid_id = super(FlatBmiHeat, self).get_var_grid(var_name)
-        shape = super(FlatBmiHeat, self).get_grid_shape(grid_id, numpy.array([0,0]))
+        shape = super(FlatBmiHeat, self).get_grid_shape(grid_id, reserve_grid_shape(self, grid_id))
         reshaped = numpy.reshape(src, shape)
         super(FlatBmiHeat, self).set_value(var_name, reshaped)
-
-    def get_value(self, var_name, dest):
-        val = super(FlatBmiHeat, self).get_value(var_name, dest)
-        numpy.copyto(src=val.flatten(), dst=dest)
-        return dest
-
-    def get_grid_shape(self, grid_id, dest=None):
-        val = super(FlatBmiHeat, self).get_grid_shape(grid_id, dest)
-        if dest is None:
-            return numpy.array(val)
-        numpy.copyto(src=val, dst=dest)
-        return dest
-
-    def get_grid_spacing(self, grid_id, dest):
-        val = super(FlatBmiHeat, self).get_grid_spacing(grid_id, dest)
-        numpy.copyto(src=val, dst=dest)
-        return dest
-
-    def get_grid_origin(self, grid_id, dest):
-        val = super(FlatBmiHeat, self).get_grid_origin(grid_id, dest)
-        numpy.copyto(src=val, dst=dest)
-        return dest
 
 
 class FlatLegacyBmiHeat(BmiHeat):
@@ -47,13 +22,28 @@ class FlatLegacyBmiHeat(BmiHeat):
 
     """
     def set_value(self, var_name, src):
-        shape = super(FlatLegacyBmiHeat, self).get_grid_shape(super(FlatLegacyBmiHeat, self).get_var_grid(var_name))
+        grid_id = super(FlatLegacyBmiHeat, self).get_var_grid(var_name)
+        shape = super(FlatLegacyBmiHeat, self).get_grid_shape(grid_id, reserve_grid_shape(self, grid_id))
         reshaped = numpy.reshape(src, shape)
         super(FlatLegacyBmiHeat, self).set_value(var_name, reshaped)
 
     def get_value(self, var_name):
-        val = super(FlatLegacyBmiHeat, self).get_value(var_name)
+        dest = reserve_values(self, var_name)
+        val = super(FlatLegacyBmiHeat, self).get_value(var_name, dest)
         return val.flatten()
 
     def get_value_at_indices(self, var_name, indices):
-        return super(FlatLegacyBmiHeat, self).get_value_at_indices(var_name, indices).flatten()
+        dest = reserve_values_at_indices(self, var_name, indices)
+        return super(FlatLegacyBmiHeat, self).get_value_at_indices(var_name, dest, indices).flatten()
+
+    def get_grid_shape(self, grid_id):
+        dest = reserve_grid_shape(self, grid_id)
+        return super(FlatLegacyBmiHeat, self).get_grid_shape(grid_id, dest)
+
+    def get_grid_spacing(self, grid_id):
+        dest = reserve_grid_padding(self, grid_id)
+        return super(FlatLegacyBmiHeat, self).get_grid_spacing(grid_id, dest)
+
+    def get_grid_origin(self, grid_id):
+        dest = reserve_grid_padding(self, grid_id)
+        return super(FlatLegacyBmiHeat, self).get_grid_origin(grid_id, dest)
