f3323b9f37c07fa990a0c079b985f92246f595ab
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2023-02-08 14:56:28 +0100

    Add image with `run-bmi-server --bmi-version 0.2 ...`

--- test/heat-images/heat_tester.py
+++ test/heat-images/heat_tester.py
@@ -5,10 +5,11 @@ from textwrap import dedent
 import numpy as np
 
 from grpc4bmi.bmi_client_docker import BmiClientDocker
+from grpc4bmi.reserve import reserve_values
 
 def test_heat(tmp_path: Path):
-    config = tmp_path / 'config.yaml'
-    config.write_text(dedent("""\
+    # Config file for Python based heat model
+    py_config = dedent("""\
         shape:
         - 6
         - 8
@@ -19,11 +20,25 @@ def test_heat(tmp_path: Path):
         - 0.0
         - 0.0
         alpha: 1.0
-    """))
+    """)
+    # Config file for C/C++ based heat model
+    c_config = '1.5, 8.0, 7, 6'
 
-    image = 'heat:py-0.2'
-    # image = 'heat:py-2.0'
-    # image = 'heat:py-2.0-pb4'
+
+    image, config_body = (
+        # Comment out line you want to run
+    # 'heat:py-0.2',py_config
+    'heat:py-0.2-legacy',py_config
+    # 'heat:py-2.0',py_config
+    # 'heat:py-2.0-pb4',py_config
+    # 'heat:cxx-bmi20-pb3', c_config
+    # 'heat:c-bmi20-pb3', c_config
+    )
+
+    config = tmp_path / 'config.yaml'
+    config.write_text(config_body)
+
+    print(image)
     model = BmiClientDocker(image, work_dir=str(tmp_path), delay=1)
 
     model.initialize(str(config))
@@ -33,10 +48,12 @@ def test_heat(tmp_path: Path):
 
     model.update()
 
-    values = model.get_value('plate_surface__temperature', np.zeros(48))
+    var_name = 'plate_surface__temperature'
+    output = reserve_values(model, var_name)
+    values = model.get_value(var_name, output)
     print(values)
 
     model.finalize()
     # assert 1==0
 
-test_heat(tmp_path=Path('/tmp/b'))
+test_heat(tmp_path=Path('/tmp'))
