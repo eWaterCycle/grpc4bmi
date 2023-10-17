92cb27ce9b432cca470b1fde66c3584e955c0383
sverhoeven <s.verhoeven@esciencecenter.nl>
2023-10-17 16:34:18 +0200

    Trying to convert repl calls into c
    Need to add handle_julia_exception from https://blog.esciencecenter.nl/10-examples-of-embedding-julia-in-c-c-66282477e62c
    to get better error
    
    Got error:
    
    1.4142135623730951
    [7] signal (11.1): Segmentation fault
    in expression starting at none:0
    jl_lookup_generic_ at /cache/build/default-amdci5-5/julialang/julia-release-1-dot-9/src/gf.c:2819 [inlined]
    ijl_apply_generic at /cache/build/default-amdci5-5/julialang/julia-release-1-dot-9/src/gf.c:2936
    jl_apply at /cache/build/default-amdci5-5/julialang/julia-release-1-dot-9/src/julia.h:1880 [inlined]
    ijl_call1 at /cache/build/default-amdci5-5/julialang/julia-release-1-dot-9/src/jlapi.c:230
    main at /usr/local/bin/run_bmi_server (unknown line)
    __libc_start_main at /lib/x86_64-linux-gnu/libc.so.6 (unknown line)
    _start at /usr/local/bin/run_bmi_server (unknown line)
    Allocations: 2060272 (Pool: 2059240; Big: 1032); GC: 3
    Segmentation fault

--- test/heat-images/c-julia/run_bmi_server.cc
+++ test/heat-images/c-julia/run_bmi_server.cc
@@ -6,25 +6,42 @@
 
 using namespace std;
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     jl_init();
     {
-         {
-        // Simple running Julia code
-        jl_eval_string("x = sqrt(2.0)");
-        jl_eval_string("print(x)");
-         }
-         {
-        jl_eval_string("import Heat");
+        {
+            // Simple running Julia code
+            jl_eval_string("x = sqrt(2.0)");
+            jl_eval_string("print(x)");
         }
         {
-          jl_function_t *initialize = jl_get_function(jl_main_module, "Heat.BMI.initialize");
-          jl_value_t* path = jl_cstr_to_string("/usr/local/share/bmi/heat.toml");
-          jl_value_t *model = jl_call1(initialize, path);
-          jl_function_t *get_component_name = jl_get_function(jl_main_module, "Heat.BMI.get_component_name");
-          jl_value_t *name = jl_call1(get_component_name, model);
-          cout << jl_string_ptr(name) << endl;
+        }
+        {
+
+            /*
+            In Julia repl:
+
+            import BasicModelInterface as BMI
+            import Heat
+            # https://github.com/csdms/bmi-example-julia/raw/main/example/heat.toml
+            model = BMI.initialize(Heat.Model, "/usr/local/share/heat.toml")
+            BMI.get_component_name(model)
+
+            */
+
+            jl_eval_string("import BasicModelInterface as BMI");
+            jl_eval_string("import Heat");
+            jl_value_t *model = jl_eval_string("BMI.initialize(Heat.Model, \"/usr/local/share/heat.toml\")");
+            // jl_function_t *initialize = jl_get_function(jl_main_module, "BMI.initialize");
+            // jl_value_t *modelt = jl_eval_string("Heat.Model");
+            // jl_value_t *path = jl_cstr_to_string("/usr/local/share/heat.toml");
+            // jl_value_t *model = jl_call2(initialize, modelt, path);
+            jl_function_t *get_component_name = jl_get_function(jl_main_module, "BMI.get_component_name");
+            jl_value_t *name = jl_call1(get_component_name, model);
+            cout << jl_string_ptr(name) << endl;
+
+            // TODO move initialize and get_component_name to a bmi interface
         }
 
         // Bmi *model = (Bmi *) malloc(sizeof(Bmi));
